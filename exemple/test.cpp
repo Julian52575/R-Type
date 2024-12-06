e3e3d7b9373be3c2c9fd68cbb6caaaa098db3277
Xekko <eliott1.mathieu@epitech.eu>
2024-12-06 17:49:05 +0100

    [+] First version of server

--- exemple/test.cpp
+++ exemple/test.cpp
@@ -1,23 +1,73 @@
 #include <array>
 #include <iostream>
 #include <asio.hpp>
-#include "UserConnexion/UserConnexion.hpp"
+#include <rengine/RengineNetwork.hpp>
 
-using asio::ip::udp;
+struct Communication {
+    enum Type : uint16_t {
+        NOOP = 0,
+        ConnexionDetail = 1,
+        JsonDetail = 2,
+        EntityAction = 3,
+        EntityInfo = 4,
+    };
 
-enum class CustomMsgTypes : uint32_t {
-  ServerConnexionRequest,
-  Message
+    union main {
+        enum NOOPPrecision : uint16_t {
+            NOOP = 0,
+        };
+
+        enum ConnexionDetailPrecision : uint16_t {
+            ClientConnexion = 0,
+            ClientDisconnect = 1,
+            ServerAccept = 2,
+            ServerDisconnect = 3,
+            ClientReady = 4,
+            ClientEntityRequest = 5,
+            PlayableEntityInGameId = 6,
+            ServerStop = 7,
+        };
+
+        enum JsonDetailPrecision : uint16_t {
+            GamePartLevel = 0,
+            GamePartEntity = 1,
+            GamePartPlugin = 2,
+        };
+
+        enum EntityActionPrecision : uint16_t {
+            MoveUp = 0,
+            MoveDown = 1,
+            MoveRight = 2,
+            MoveLeft = 3,
+            Shoot1 = 4,
+            Shoot2 = 5,
+            Shoot3 = 6,
+            Dodge = 7,
+            Barrier = 8,
+            Death = 9,
+            Ultimate = 0x0A
+        };
+
+        enum EntityInfoPrecision : uint16_t {
+            InfoAll = 0,
+            InGameAndConfigurationId = 1,
+            InfoPosition = 2,
+            InfoHP = 3,
+            InfoMaxHp = 4,
+            InfoSpeed = 5,
+            InfoMaxSpeed = 6,
+            InfoDirection = 7,
+        };
+    };
+
+    struct TypeDetail {
+        Type type;
+        uint16_t precision;
+    };
 };
-void handleMessage(Message<CustomMsgTypes> msg, ClientConnection<CustomMsgTypes> &client) {
-  std::cout << "Message received: " << msg << std::endl;
-  if (msg.header.type == CustomMsgTypes::Message) {
-    char data[256];
-    msg >> data;
-    std::cout << "Data: " << data << std::endl;
-  } else {
-    std::cerr << "Unknown message type" << std::endl;
-  }
+using asio::ip::udp;
+void handleMessage(Message<Communication::TypeDetail> msg, ClientConnection<Communication::TypeDetail> &client) {
+  std::cout << "Message received: " << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -27,19 +77,16 @@ int main(int argc, char* argv[])
     std::cerr << "Usage: client <host>" << std::endl;
     return 1;
   }
-  ClientConnection<CustomMsgTypes> client(argv[1], std::stoi(argv[2]));
+  ClientConnection<Communication::TypeDetail> client(argv[1], std::stoi(argv[2]));
+  Message<Communication::TypeDetail> msg;
+
+  msg.header.type = {Communication::Type::ConnexionDetail, Communication::main::ConnexionDetailPrecision::ClientConnexion};
+  msg.header.size = 0;
+  client.Send(msg);
 
   for (std::string line; std::getline(std::cin, line);) {
-    for (std::optional<Message<CustomMsgTypes>> msg = client.Receive(); msg; msg = client.Receive()) {
+    for (std::optional<Message<Communication::TypeDetail>> msg = client.Receive(); msg; msg = client.Receive()) {
       handleMessage(*msg, client);
-    }
-    if (line == "send") {
-      Message<CustomMsgTypes> msg;
-      msg.header.type = CustomMsgTypes::Message;
-      msg.header.size = 0;
-      char data[256] = "Hello from client!";
-      msg << data;
-      client.Send(msg);
     }
   }
   } catch(const std::exception& e) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,90 @@
 #include <rengine/Rengine.hpp>
-#include <SFML/Main.hpp>
+#include <rengine/RengineNetwork.hpp>
 
-int main(void) {
-    GraphicalCore core;
-    core.MakeEntity("entities/player.json");
-    core.MakeEntity("entities/projectile.json");
-    core.MakeEntity("entities/Message.json");
-    // // core.MakeBackground();
-    // core.MakeParallaxBackground("assets/images/parallax background/plx-1.png", 0.0f);
-    // core.MakeParallaxBackground("assets/images/parallax background/plx-2.png", -15.f);
-    // core.MakeParallaxBackground("assets/images/parallax background/plx-3.png", -30.f);
-    // core.MakeParallaxBackground("assets/images/parallax background/plx-4.png", -45.f);
-    // core.MakeParallaxBackground("assets/images/parallax background/plx-5.png", -60.f);
-    core.run();
-    return 0;
+struct Communication {
+    enum Type : uint16_t {
+        NOOP = 0,
+        ConnexionDetail = 1,
+        JsonDetail = 2,
+        EntityAction = 3,
+        EntityInfo = 4,
+    };
+
+    union main {
+        enum NOOPPrecision : uint16_t {
+            NOOP = 0,
+        };
+
+        enum ConnexionDetailPrecision : uint16_t {
+            ClientConnexion = 0,
+            ClientDisconnect = 1,
+            ServerAccept = 2,
+            ServerDisconnect = 3,
+            ClientReady = 4,
+            ClientEntityRequest = 5,
+            PlayableEntityInGameId = 6,
+            ServerStop = 7,
+        };
+
+        enum JsonDetailPrecision : uint16_t {
+            GamePartLevel = 0,
+            GamePartEntity = 1,
+            GamePartPlugin = 2,
+        };
+
+        enum EntityActionPrecision : uint16_t {
+            MoveUp = 0,
+            MoveDown = 1,
+            MoveRight = 2,
+            MoveLeft = 3,
+            Shoot1 = 4,
+            Shoot2 = 5,
+            Shoot3 = 6,
+            Dodge = 7,
+            Barrier = 8,
+            Death = 9,
+            Ultimate = 0x0A
+        };
+
+        enum EntityInfoPrecision : uint16_t {
+            InfoAll = 0,
+            InGameAndConfigurationId = 1,
+            InfoPosition = 2,
+            InfoHP = 3,
+            InfoMaxHp = 4,
+            InfoSpeed = 5,
+            InfoMaxSpeed = 6,
+            InfoDirection = 7,
+        };
+    };
+
+    struct TypeDetail {
+        Type type;
+        uint16_t precision;
+    };
+};
+
+void handleMessage(Message<Communication::TypeDetail> &msg, const asio::ip::udp::endpoint &endpoint ,ServerConnection<Communication::TypeDetail> &server) {
+    std::cout << endpoint << std::endl;
+}
+
+int main(int argc, char **argv) {
+    try {
+        if (argc < 2) {
+            std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+            return 84;
+        }
+        int port = std::stoi(argv[1]);
+        ServerConnection<Communication::TypeDetail> server(port);
+
+
+        while (1) {
+            for (std::optional<std::pair<asio::ip::udp::endpoint, Message<Communication::TypeDetail>>> msg = server.Receive(); msg; msg = server.Receive()) {
+                handleMessage(msg->second, msg->first, server);
+            }
+        }
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return 84;
+    }
 }
