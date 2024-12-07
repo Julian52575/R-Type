9501d56211566f27af4f4ae6753376d519b5c8f3
julian <julian.bottiglione@epitech.eu>
2024-12-07 20:55:00 +0100

    [fix] Installing conan if not already installed

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 LIB_NAME=librengine.so
 
+if ! conan --version > /dev/null; then
+    echo "Installing conan with pip..."
+    if ! sudo pip install conan --no-warn-conflicts; then
+        exit 84
+    fi
+fi
+
 conan install . --output-folder=build --build=missing
 cmake -B build -DCMAKE_TOOLCHAIN_FILE="build/conan_toolchain.cmake" -DCMAKE_BUILD_TYPE=RELEASE
 if cmake --build build ; then
