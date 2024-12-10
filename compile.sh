dff8406b8935ee165a6bc45449d8d56ef29f286f
Xekko <eliott1.mathieu@epitech.eu>
2024-12-10 14:36:47 +0100

    change compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 LIB_NAME=librengine.so
 
+if ! dpkg -s python3-venv > /dev/null 2>&1; then
+    echo "python3-venv is not installed. Installing..."
+    sudo apt-get install python3-venv
+fi
+
+python3 -m venv venvconan
+source venvconan/bin/activate
+pip3 install conan
+
 if ! conan --version > /dev/null; then
     echo "Installing conan with pip..."
     if ! sudo pip install conan --no-warn-conflicts; then
@@ -16,3 +25,6 @@ if cmake --build build ; then
     echo "Succesfully built $LIB_NAME !"
 fi
 #cpack --config build/CPackConfig.cmake
+
+deactivate
+rm -rf venvconan
