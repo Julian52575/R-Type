04e95f23f764dc7ed41622b94ef03c24ada48909
antoine <antoine.weisse@epitech.eu>
2025-01-08 18:18:25 +0100

    [add] space_ background

--- src/main.cpp
+++ src/main.cpp
@@ -33,58 +33,26 @@ static void processInputs(std::shared_ptr<Rengine::Graphics::AWindow> &window, R
     inputManager.clear();
 }
 
-/* Component Function */
-static void processActions(const Rengine::ECS& ecs, RType::Components::Action& actionComponent, Rengine::Entity& entity)
-{
-    RType::Components::Stats& stats = entity.getComponent<RType::Components::Stats>();
-    RType::Components::Position& pos = entity.getComponent<RType::Components::Position>();
-    auto currentPos = pos.getVector2D();
-    auto newPos = pos.getVector2D();
-
-    for (auto it : actionComponent) {
-        switch (it.type) {
-            // Move
-            case RType::Network::EntityActionType::EntityActionTypeMove:
-                newPos.x +=  it.data.moveVelocity.x;
-                newPos.y +=  it.data.moveVelocity.y;
-                pos.setX(newPos.x);
-                pos.setY(newPos.y);
-                break;
-            // Shoot1 -> 3
-            case RType::Network::EntityActionType::EntityActionTypeShoot1:
-                std::cout << "Shoot1" << std::endl;
-                break;
-            case RType::Network::EntityActionType::EntityActionTypeShoot2:
-                std::cout << "Shoot2" << std::endl;
-                break;
-            case RType::Network::EntityActionType::EntityActionTypeShoot3:
-                std::cout << "Shoot3" << std::endl;
-                break;
-
-            default:
-                break;
-        }  // switch it.type
-    }  // for it : actionComponent
-    actionComponent.clear();
-}
-
 static void InputSystem(Rengine::Entity& entity,float deltatime)
 {
-    RType::Components::Stats& stats = entity.getComponent<RType::Components::Stats>();
-    RType::Components::Position& pos = entity.getComponent<RType::Components::Position>();
-    RType::Components::Action& actionComponent = entity.getComponent<RType::Components::Action>();
+    auto stats = entity.getComponentNoExcept<RType::Components::Stats>();
+    auto pos = entity.getComponentNoExcept<RType::Components::Position>();
+    auto  actionComponent = entity.getComponentNoExcept<RType::Components::Action>();
+    if (!stats || !pos || !actionComponent) {
+        return;
+    }
 
-    auto currentPos = pos.getVector2D();
-    auto newPos = pos.getVector2D();
+    auto currentPos = pos->get().getVector2D();
+    auto newPos = pos->get().getVector2D();
 
-    for (auto it : actionComponent) {
+    for (auto it : actionComponent->get()) {
         switch (it.type) {
             // Move
             case RType::Network::EntityActionType::EntityActionTypeMove:
                 newPos.x +=  it.data.moveVelocity.x * deltatime;
                 newPos.y +=  it.data.moveVelocity.y * deltatime;
-                pos.setX(newPos.x);
-                pos.setY(newPos.y);
+                pos->get().setX(newPos.x);
+                pos->get().setY(newPos.y);
                 break;
             // Shoot1 -> 3
             case RType::Network::EntityActionType::EntityActionTypeShoot1:
@@ -99,27 +67,21 @@ static void InputSystem(Rengine::Entity& entity,float deltatime)
 
             default:
                 break;
-        }  // switch it.type
-    }  // for it : actionComponent
-    actionComponent.clear();
+        }
+    }
+    actionComponent->get().clear();
 }
-
 
 static void renderSpriteSystem(Rengine::Entity& entity)
 {
-    auto pos = entity.getComponent<RType::Components::Position>();
-    auto spriteComponent = entity.getComponent<RType::Components::Sprite>();
+    auto pos = entity.getComponentNoExcept<RType::Components::Position>();
+    auto spriteComponent = entity.getComponentNoExcept<RType::Components::Sprite>();
 
-    spriteComponent.renderSprite(pos.getVector2D());
-}
+    if (!pos || !spriteComponent) {
+        return;
+    }
 
-
-
-static void processSprite(const Rengine::ECS& ecs, RType::Components::Sprite& spriteComponent, Rengine::Entity& entity)
-{
-    auto pos = entity.getComponent<RType::Components::Position>();
-
-    spriteComponent.renderSprite(pos.getVector2D());
+    spriteComponent->get().renderSprite(pos->get().getVector2D());
 }
 
 /* Set Up */
@@ -127,12 +89,11 @@ inline static void setUpEcs(Rengine::ECS& ecs)
 {
     ecs.registerComponent<RType::Components::Stats>();
     ecs.registerComponent<RType::Components::Action>();
-    ecs.setComponentFunction<RType::Components::Action>(processActions);
     ecs.registerComponent<RType::Components::Position>();
     ecs.registerComponent<RType::Components::Sprite>();
-    ecs.setComponentFunction<RType::Components::Sprite>(processSprite);
 
 }
+
 inline static void setUpPlayer(Rengine::Entity& player, Rengine::Graphics::GraphicManager& graphicManager)
 {
     RType::Config::EntityConfig config("assets/entities/playerDefault.json");
@@ -141,6 +102,16 @@ inline static void setUpPlayer(Rengine::Entity& player, Rengine::Graphics::Graph
     player.addComponent<RType::Components::Stats>(config.getStats());
     player.addComponent<RType::Components::Sprite>(graphicManager, config.getSprite().getConfig());
     player.addComponent<RType::Components::Position>(10, 0);
+}
+
+inline static void setUpBackground(Rengine::Entity& background, Rengine::Graphics::GraphicManager& graphicManager)
+{
+    RType::Config::EntityConfig config("assets/entities/space_background.json");
+
+    std::cout << "Background" << std::endl;
+
+    background.addComponent<RType::Components::Sprite>(graphicManager, config.getSprite().getConfig());
+    background.addComponent<RType::Components::Position>(0, 0);
 }
 
 enum Scene {
@@ -157,8 +128,12 @@ int main(void)
     Rengine::ECS ecs;
     setUpEcs(ecs);
 
+    Rengine::Entity& background = ecs.addEntity();
+    // setUpBackground(background, graphicManager);
+
     Rengine::Entity& player = ecs.addEntity();
     setUpPlayer(player, graphicManager);
+
 
     RType::SceneManager<Scene> sceneManager;
 
@@ -181,6 +156,7 @@ int main(void)
     });
 
     sceneManager.addEntityToScene(SceneGame, player);
+    sceneManager.addEntityToScene(SceneGame, background);
 
     sf::Clock clock;
     while (window->isOpen()) {
