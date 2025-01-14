6f48b43dad76c45f74d9064e4e618bc2551b04ec
Néo Grandval <neo.burtin@epitech.eu>
2025-01-14 17:05:36 +0100

    [~] link sfml

--- compile.ps1
+++ compile.ps1
@@ -1,6 +1,9 @@
 conan install . --output-folder=build --build=missing
-cmake -B build -G "Visual Studio 17 2022" `
+
+cmake -B build -G "Visual Studio 17 2022" -A x64 `
       -DCMAKE_TOOLCHAIN_FILE="build/conan_toolchain.cmake" `
-      -DCMAKE_BUILD_TYPE=Release
-cmake --build build
+      -DCMAKE_BUILD_TYPE=Release `
+
+cmake --build build --config Release
+
 cpack --config build/CPackConfig.cmake
