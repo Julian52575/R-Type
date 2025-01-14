23311a665d0f0b3399dc980ee2738b288316c150
Néo Grandval <neo.burtin@epitech.eu>
2025-01-14 14:40:55 +0100

    [~] cmakefiles compile with json and msvc

--- compile.ps1
+++ compile.ps1
@@ -1,8 +1,6 @@
 conan install . --output-folder=build --build=missing
-cmake -B build -G "Ninja" -DCMAKE_TOOLCHAIN_FILE="build/conan_toolchain.cmake" -DCMAKE_BUILD_TYPE=RELEASE
-$Env:CC='C:\ProgramData\mingw64\mingw64\bin\gcc.exe'
-$Env:CXX='C:\ProgramData\mingw64\mingw64\bin\g++.exe'
-# -I'C:\ProgramData\Include\
-$Env:CXXFLAGS="-DCMAKE_COMPILATION_ -iquote ../gameEngine/ "
+cmake -B build -G "Visual Studio 17 2022" -A x64 `
+      -DCMAKE_TOOLCHAIN_FILE="build/conan_toolchain.cmake" `
+      -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 cpack --config build/CPackConfig.cmake
