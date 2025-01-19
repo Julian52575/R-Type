6f02a4c44d9457cb24b57aad4b98e2cba789a7f9
julian <julian.bottiglione@epitech.eu>
2025-01-19 10:42:49 +0100

    Squashed 'assets/' changes from ec57b49..52ee338
    52ee338 [add] id.json
    6fde63b [fix] Final adjustments for yoshi
    a1905ec [add] Yoshi bgm
    28eae3f [PATCH NOTE] Misc stats adjustement
    e86636e [Patch note] Corrected Missiles damages and speed
    3e55592 [fix] Added attack for yoshi chara
    e4300bb [fix] Hitbox and origin
    acf2167 [fix] Birg anim
    a7ebed2 [fix] fatBird anim
    8fc7935 [fix] frameDuration
    e97e376 [fix] Yoshi images scale
    b455ebb [fix] Forest background path
    3ed4500 [add] Attacks, entities and scripts for yoshi
    dc81a4a [add] Yoshi's images
    fbfe982 [fix] Deleted old file && Renamed playerDefault
    
    git-subtree-dir: assets
    git-subtree-split: 52ee3382a1ea693901ad10c1f51c5b54a3b5124f

---
+++ scripts/dvd.lua
@@ -0,0 +1,46 @@
+EntityActionTypeMove = 0x00
+EntityActionTypeShoot1 = 0x01
+EntityActionTypeShoot2 = 0x02
+EntityActionTypeShoot3 = 0x03
+EntityActionTypeDodge = 0x04
+EntityActionTypeBarrier = 0x05
+EntityActionTypeDeath = 0x06
+EntityActionTypeUltimate = 0x07
+EntityActionTypeInvalid = 0xff
+
+math.randomseed(os.time())
+
+DirectionUp = -1
+DirectionDown = 1
+DirectionLeft = -1
+DirectionRight = 1
+
+x_dir = DirectionDown
+y_dir = DirectionLeft
+
+function updateDeltatime(deltatime)  -- float deltatime
+end  -- return values are ignored
+
+function move(pos_x, pos_y)
+
+    if pos_x <= 10.0
+    then
+        x_dir = DirectionRight
+    elseif pos_x >= 1900.0
+    then
+        x_dir = DirectionLeft
+    end
+
+    if pos_y <= 10.0
+    then
+        y_dir = DirectionDown
+    elseif pos_y >= 1070.0
+    then
+        y_dir = DirectionUp
+    end
+    return {80.0 * x_dir, 80.0 * y_dir}
+end
+
+function shoot(shoot1Deltatime, shoot2Deltatime, shoot3Deltatime)
+    return EntityActionTypeShoot1
+end
---
+++ scripts/smartShoot.lua
@@ -0,0 +1,37 @@
+EntityActionTypeMove = 0x00
+EntityActionTypeShoot1 = 0x01
+EntityActionTypeShoot2 = 0x02
+EntityActionTypeShoot3 = 0x03
+EntityActionTypeDodge = 0x04
+EntityActionTypeBarrier = 0x05
+EntityActionTypeDeath = 0x06
+EntityActionTypeUltimate = 0x07
+EntityActionTypeInvalid = 0xff
+time = 0.0
+
+function updateDeltatime(deltatime)  -- float deltatime
+    time = time + deltatime
+end  -- return values are ignored
+
+function move(pos_x, pos_y)
+    return {0, 0} -- return x, y  -- float get converted to int automatically by the api
+end
+
+function shoot(shoot1Deltatime, shoot2Deltatime, shoot3Deltatime)
+    if shoot3Deltatime > 5.0
+    then
+        return EntityActionTypeShoot3
+    end
+
+    if shoot2Deltatime > 3.0
+    then
+        return EntityActionTypeShoot2
+    end
+
+    if shoot1Deltatime > 1.0
+    then
+        return EntityActionTypeShoot1
+    end
+
+    return EntityActionTypeInvalid
+end
